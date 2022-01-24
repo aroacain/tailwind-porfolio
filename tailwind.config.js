37c748743182fea3b90b949ed1717efb3807250a
Aroa Cain <aroacain@Aroas-Mac-mini.local>
2022-01-24 17:10:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'pablue': '#F0F0F0',
+        'padark': '#38404E',
+        'pabrown': '#D4C5B8',
+        'papink': '#F1BABB',
+        'pablush': '#D37879',
+      },
+      fontFamily: {
+        'lustria': ["'Lustria'", 'serif'],
+        'pacifico': ["'Pacifico'", 'cursive'],
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
